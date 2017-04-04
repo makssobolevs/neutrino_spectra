588f242723af956b092c7b89ec78d12f8bf62fc9
makssobolevs <makssobolevs@gmail.com>
2017-04-04 12:14:51 +0300

    changes

--- calculation/compare_yield.py
+++ calculation/compare_yield.py
@@ -11,34 +11,3 @@ independent = endf_loader.get_cumulative_yields(element, constants.Database.JEND
 for el in independent:
     print("ind:{} -- cfy:{}".format(el['y'], cfy.get_cfy_for_z_a(el['z'], el['a'])))
 
-# t = 1 * 365 * 24 * 3600
-#
-# for el in main.data:
-#     chain = [el]
-#     chain += el['chain']
-#
-#     ii = 0
-#     for e in chain:
-#         ii += 1
-#         if e['hl'] == float("inf"):
-#             break
-#
-#     last = chain[ii - 1]
-#
-#     def f_last():
-#         return bateman_solving(chain, ii - 1, t)
-#
-#     cfy_calc = f_last()
-#     cfy_table = cfy.get_cfy_for_z_a(last['z'], last['a'])
-#
-#     print("calc:{}, table:{}, diff:{}".format(cfy_calc, cfy_table, cfy_table - cfy_calc))
-#
-#
-# al = []
-#
-# for el in main.data:
-#     al += el
-#     al += el['chain']
-#
-# print(len(al))
-
